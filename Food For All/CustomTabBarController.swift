52f5e3ddc16a06329be2a2f0d833c859785b4af8
Daniel Jones <messyjones@gmail.com>
2017-02-01 13:36:55 -0500

    tab bar pushes the modal vc onto the screen

--- Food For All/CustomTabBarController.swift
+++ Food For All/CustomTabBarController.swift
@@ -9,9 +9,14 @@
 import UIKit
 
 class CustomTabBarController: UITabBarController {
+    struct Titles {
+        static let creationVC: String = "creationVC"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         viewControllers = [firstVC, secondVC, thirdVC]
+        delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +28,20 @@ class CustomTabBarController: UITabBarController {
         let item = UITabBarItem(title: nil, image: image, tag: tag)
         item.imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: -5, right: 0)
         return item
+    }
+}
+
+//delegate methods
+extension CustomTabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController.title == Titles.creationVC {
+            //The creationVC should not show the tab bar, we want it to present the creation stuff over the whole screen, like instagram/flipagram does, and then they can hit X in the corner to exit.
+            let vcToPresent = CreationViewController() //need to make a new VC, can't re-present the creationVC or else crash
+            present(vcToPresent, animated: true, completion: nil)
+            return false
+        }
+        
+        return true
     }
 }
 
@@ -48,7 +67,8 @@ extension CustomTabBarController {
     }
     
     var secondVC: UIViewController {
-        let vc = UIViewController()
+        let vc = UIViewController() //placholder vc, so we can have a modal segue to creationVC
+        vc.title = Titles.creationVC
         vc.tabBarItem = secondTabBarItem
         return vc
     }
