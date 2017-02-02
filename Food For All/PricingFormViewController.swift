7a25c7cae7ed1c438f3640b4b02894be393fbe0a
Daniel Jones <messyjones@gmail.com>
2017-02-02 17:28:26 -0500

    the pricing slider is looking and working nicely

--- Food For All/PricingFormViewController.swift
+++ Food For All/PricingFormViewController.swift
@@ -29,6 +29,10 @@ extension PricingFormViewController {
         priceRow.configure { (row) in
             row.cell.slider.maximumValue = 100
             row.cell.slider.minimumValue = 0
+            row.onValueChanged({ (value: Float) in
+                let roundedValue = Int(value)
+                row.cell.displayLabel.text = roundedValue.toString + " $"
+            })
         }
         append(rows:[priceRow], headerTitle: "Pricing")
     }
