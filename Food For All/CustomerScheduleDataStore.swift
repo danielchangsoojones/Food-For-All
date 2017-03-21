c33e7936586e4f6e41ed48f742fc7697198c00dd
Daniel Jones <messyjones@gmail.com>
2017-03-21 17:41:18 -0400

    the contracts get sent to groupme now also

--- Food For All/CustomerScheduleDataStore.swift
+++ Food For All/CustomerScheduleDataStore.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Mixpanel
+import Alamofire
 
 class CustomerScheduleDataStore {
     func save(contract: Contract) {
@@ -20,5 +21,18 @@ class CustomerScheduleDataStore {
     
     fileprivate func saveContractMetric(contract: Contract) {
         Mixpanel.mainInstance().track(event: "Contract", properties: ["status" : "in-progress", "freelancer" : contract.gig.creator.fullName ?? "Unknown"])
+        sendContractToGroupMe(contract: contract)
+    }
+    
+    fileprivate func sendContractToGroupMe(contract: Contract) {
+        var configuration = Configuration()
+        if configuration.environment == .Production {
+            let url = "https://maker.ifttt.com/trigger/new-contract/with/key/bmku_IppapnZ3eewT54mzi"
+            let fullName = User.current()?.fullName ?? ""
+            let parameters: Parameters = ["value1" : fullName, "value2" : contract.gig.creator.fullName ?? "Unknown", "value3" : contract.gig.title]
+            Alamofire.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default).responseData(completionHandler: { (response) in
+                print(response)
+            })
+        }
     }
 }
